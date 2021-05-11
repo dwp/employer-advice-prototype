bd7c7199d176a235726d234f7b02cd495ace79ca
Adam Hindle <adam.hindle@engineering.digital.dwp.gov.uk>
2021-05-11 15:13:37 +0100

    First commit
    First commit

---
+++ app/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+//Radio button redirect package
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
