e38fb8058d29b91b71909162823547c4445b17f1
Adam Hindle <adam.hindle@engineering.digital.dwp.gov.uk>
2021-06-29 17:39:43 +0100

    Added dayjs package for date manipulation, setup test scenario for how long off sick question

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const dayjs = require('dayjs')
+var objectSupport = require("dayjs/plugin/objectSupport");
+dayjs.extend(objectSupport);
+
 const router = express.Router()
 
 //Radio button redirect package
@@ -6,5 +10,27 @@ const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
 // Add your routes here - above the module.exports line
+
+
+// Examples 
+router.post('/test/date-test-answer', function (req, res) {
+
+    const day = Number(req.session.data['TestDay'])
+    const month = Number(req.session.data['TestMonth'])
+    const year = Number(req.session.data['TestYear'])
+
+    const startDate = dayjs({ year : year, month : month-1, day :day})
+
+    const now = dayjs()
+
+    const daysEmployeeOffSick = now.diff(startDate, "day")
+
+    console.log(daysEmployeeOffSick)
+
+    req.session.data['daysEmployeeOffSick'] = daysEmployeeOffSick
+
+    res.redirect('/test/date-results-test')
+
+  })
 
 module.exports = router
