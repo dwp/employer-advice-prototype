ecee57263f2ee0f64f8e0b6d3db551a34e274fe8
Adam Hindle <adam.hindle@engineering.digital.dwp.gov.uk>
2021-08-05 17:17:51 +0100

    Sprint 3 - full absence through sickness journey

--- app/views/beta/sprint-4/routes.js
+++ app/views/beta/sprint-4/routes.js
@@ -15,4 +15,36 @@ router.use(radioButtonRedirect)
 //     res.redirect('test-2');
 // });
 
+router.post('/prototype-A/establishing/ssp-routing', function (req, res) {
+
+    const day = Number(req.session.data['employeeLeaveDateDayA'])
+    const month = Number(req.session.data['employeeLeaveDateMonthA'])
+    const year = Number(req.session.data['employeeLeaveDateYearA'])
+
+    const startDate = dayjs({ year : year, month : month-1, day :day})
+
+    const now = dayjs()
+
+    const daysEmployeeOffSick = now.diff(startDate, "day")
+
+    console.log(daysEmployeeOffSick)
+
+    req.session.data['daysEmployeeOffSick'] = daysEmployeeOffSick
+
+    // res.redirect('/test/date-results-test')
+
+    if (daysEmployeeOffSick <= '3') {
+        res.redirect('est-g1');
+    } else {
+        res.redirect('est-g2&3');
+    }
+
+    // {% if data['daysEmployeeOffSick'] <= '3'  %}
+
+
+    // {% elif (data['daysEmployeeOffSick'] >= '4') and (data['daysEmployeeOffSick'] <= '7') %}
+
+
+  })
+
 module.exports = router
